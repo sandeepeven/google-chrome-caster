a37b2abee7f76199c64c77916422816e8309a624
Sandeep Singh <ss.rangi47@gmail.com>
2026-04-14 08:15:33 +0530

    Initial Commit

---
+++ ChromeCaster/ChromeCasterApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChromeCasterApp.swift
+//  ChromeCaster
+//
+//  Created by Sandeep Singh on 14/04/26.
+//
+
+import SwiftUI
+
+@main
+struct ChromeCasterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
